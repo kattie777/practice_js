57207a689b25dc857530ddf987089f97e41482d1
Katia <monit.alerts22@gmail.com>
2023-12-16 01:47:29 +0200

    answers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 'use strict';
 
-console.log(NaN || 2 || undefined);
+console.log(NaN || 2 || undefined); //2
+console.log( NaN && 2 && undefined ); // NaN
+console.log( 1 && 2 && 3 ); //3
+console.log( !1 && 2 || !3 ); //!3
+console.log( 25 || null && !3 ); //25
+console.log( NaN || null || !3 || undefined || 5); //5
+console.log( NaN || null && !3 && undefined || 5); //5
+console.log( 5 === 5 && 3 > 1 || 5); //1
 
 
 
