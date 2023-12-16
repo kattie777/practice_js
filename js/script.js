f6b4f531e0d9a68abe4e9721f80ca52ce0cce183
Katia <monit.alerts22@gmail.com>
2023-12-16 21:36:21 +0200

    second level tasks

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 let st = '';
-for (i = 1; i < 7; i++) {
+for (let i = 1; i < 7; i++) {
     st = '';
-    for (j = 1; j < i + 1; j++) {
+    for (let j = 1; j < i + 1; j++) {
         st = st + '*';
     }
     console.log(st);
@@ -46,6 +46,44 @@ let arr = [];
 for (let i = 0; i < 6; i++){
     arr[i] = 5 + i;
 }
+
+//2_level first
+const arr2 = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i < arr2.length; i++) {
+    result[i] = arr2[i];
+}
+console.log(result);
+
+
+
+//2_level second
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    for (let i = 0; i < data.length; i++) {
+        if (typeof(data[i]) === 'string') {
+            data[i] = data[i] + ' - done';
+        } else if (typeof (data[i]) === 'number') {
+            data[i] = data[i] * 2;
+        }
+
+    }
+}
+
+//2_level third
+function thirdTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = data.length - 1, j = 0; i > -1; i--, j++) {
+        result[i] = data[0 + j];
+    }
+console.log(result)
+}
+
 
 
 
