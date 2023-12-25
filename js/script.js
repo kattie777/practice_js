15333376a44bcfbf72f2920a3515a39f180dee01
Katia <monit.alerts22@gmail.com>
2023-12-25 19:18:52 +0200

    Getting elements

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 'use strict';
 
+const box = document.getElementById('box');
 
+const wrapper = document.querySelectorAll('.wrapper');
+const hearts = document.querySelectorAll('.heart');
 
+const divBlack = document.createElement('div'); //создать элемент, будет отображаться только в джиес
+divBlack.classList.add('black'); //добавить элементу класс
 
+const divRose = document.createElement('div');
+divRose.classList.add('rose');
+
+const divBlue = document.createElement('div');
+divBlue.classList.add('blue');
+
+const divRed = document.createElement('div');
+divRed.classList.add('red');
+
+const divGreen = document.createElement('div');
+divGreen.classList.add('green');
+
+wrapper[0].append(divBlack); //добавить элемент в скобках в конец элемента врэппэр
+wrapper[0].prepend(divRose); 
+hearts[2].before(divBlue)
+hearts[2].after(divRed)
+hearts[3].replaceWith(divGreen)
+hearts[0].remove();
 
 
 
